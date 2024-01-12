3a8d9120c64d385ed9be0c3aa69c52916e4613de
unknown <tejukanni1113@gmail.com>
2024-01-12 18:45:46 +0530

    deleting students details in the database

--- src/main/java/com/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/studentmanagementsystem/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.studentmanagementsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,26 +18,34 @@ public class StudentController {
 
 	@Autowired
 	StudentServices ss;
-	
+
 	@PostMapping(value = "/addStudent")
 	public String addStudent(@RequestParam("id") int id,
-								@RequestParam("name") String name,
-									@RequestParam("branch") String branch) {
+			@RequestParam("name") String name,
+			@RequestParam("branch") String branch) {
 		Student st=new Student(id, name, branch);
 		String msg = ss.addStudent(st);
 		System.out.println(msg);
-		return "index";
+		return "redirect:/";
 	}
-	
+
 	@PutMapping(value = "/updateStudent")
 	public String updateStudent(@RequestParam("id") int id,
-								@RequestParam("name") String name,
-									@RequestParam("branch") String branch) {
+			@RequestParam("name") String name,
+			@RequestParam("branch") String branch) {
 		Student st = ss.getStudent(id);
 		st.setName(name);
 		st.setBranch(branch);
 		String msg = ss.updateStudent(st);
 		System.out.println(msg);
-		return "index";
+		return "redirect:/";
+	}
+
+	@DeleteMapping(value = "/deleteStudent")
+	public String deleteStudent(@RequestParam("id") int id)
+	{
+		String msg = ss.deleteStudent(id);
+		System.out.println(msg);
+		return "redirect:/";
 	}
 }
