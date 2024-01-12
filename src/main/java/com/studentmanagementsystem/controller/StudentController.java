65e0908e5e51a2d5eeeb4ea816c55e4562a4443e
unknown <tejukanni1113@gmail.com>
2024-01-12 18:07:28 +0530

    updating student details in the database

--- src/main/java/com/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/studentmanagementsystem/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.studentmanagementsystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,18 @@ public class StudentController {
 									@RequestParam("branch") String branch) {
 		Student st=new Student(id, name, branch);
 		String msg = ss.addStudent(st);
+		System.out.println(msg);
+		return "index";
+	}
+	
+	@PutMapping(value = "/updateStudent")
+	public String updateStudent(@RequestParam("id") int id,
+								@RequestParam("name") String name,
+									@RequestParam("branch") String branch) {
+		Student st = ss.getStudent(id);
+		st.setName(name);
+		st.setBranch(branch);
+		String msg = ss.updateStudent(st);
 		System.out.println(msg);
 		return "index";
 	}
